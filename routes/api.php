4d6459c1e7f0e87db47685aba9afa8830c85fd23
Tiago Murakami <trmurakami@gmail.com>
2022-01-12 11:55:17 -0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CreativeWorkController;
 use App\Http\Controllers\MusicRecordingController;
 use App\Http\Controllers\MusicAlbumController;
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CreativeWorkController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/result', function () {
     return view('result');
 });
+
+Route::resource('/creative_work', 'App\Http\Controllers\CreativeWorkController@getAll');
